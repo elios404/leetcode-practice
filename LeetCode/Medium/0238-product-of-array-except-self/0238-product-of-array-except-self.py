95ed0d0f48160403a7995dadc435b193d2bddd99
Cheon <elioscode@gmail.com>
2026-03-17 17:17:34 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,31 +1,27 @@
 """
 1. Approach
-    - Firstly, should not use `/`operator. nums.length <= 10^5, so normally if a problem gives us 1s, then we need to implement O(N log N) algorithm.
-    - If we can use `/` then it's easy. Just multiple all the numbers in the list and divide each elements to total mul value. O(2*N) time complexity. O(1) space complexity.
-    - Calculate in advance the value of 1th ~ ith and also Nth ~ ith order and if I want to find k th value, use (1th~(k-1)th) * ((k+1)th ~ Nth) 
-2. Time Complexity : O(N) - itereate for three times, O(3*N)
-3. Space Complexity : O(N) - 3 arryas which length is N are needed.
+    - Utilize a two-pass stratagy to compute prefix and suffix products without using division operator.
+    - Instead of allocating separate arrays for prefixes and suffixes, accumulate the prefix products directry into result array during the fisrt forward pass.
+    -During the second backward pass, maintain a ruunig `suffix` variable, multiplying it directly into the result array to achieve constant auxiliary space.
+2. Time Complexity : O(N) - We perform exactly two linear passes over the input array.
+3. Space Complexity : O(1) - The output array does not count towards auxiliary space, and we only use constant integer variables (`prefix` and `suffix`).
 """
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        l = [1] 
-        n = 1
-        for num in nums:
-            n *= num
-            l.append(n)
+        n = len(nums)
+        res = [1]*n
+
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
         
-        reverse_l = [1]  
-        n = 1
-        for num in reversed(nums):
-            n *= num
-            reverse_l.append(n)
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
         
-        length = len(nums)
-        ret = []
-        for i in range(length):
-            # one small problem is that I can't fully understand the index of the list
-            ret.append(l[i] * reverse_l[length-i-1])
-        return ret
+        return res
 
             
         
