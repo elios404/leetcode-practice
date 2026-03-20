79add9bbcac79e9416b9b51cb70476ab61dd9b5f
Cheon <elioscode@gmail.com>
2026-03-20 10:59:56 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.py
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.py
@@ -1,25 +1,16 @@
 """
 1. Approach
-    - Use two pointer algorithm
-    -
-    -
-2. Time Complexity : O(N) - Iterate array twice in worse case
-3. Space Complexity : O(1) - Only need constant variable `zero` and `idx` for two pointer
+    - Utilize a Fast/Slow Two-Pointer approach to partition the array in-place.
+    - As teh loop iterates, whenever a non-zero element is found, it is swapped with the element at the `insert_pos` (the slow pointer).
+    - The `insert_pos` is then incremented, guaranteering that all elements to the left of `insert_pos` are non-zero, and all zeros are natually pushed to the rigth.
+2. Time Complexity : O(N) - A single linear traversal of the array
+3. Space Complexity : O(1) - In-place modification using a single integer pointer variable.
 """
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        zero = 0
-        idx = 0
-        # O(N)
-        while idx< len(nums):
-            if nums[idx] != 0:
-                # O(N)?
-                while zero < idx and nums[zero] != 0:
-                    zero += 1
-                nums[zero], nums[idx] = nums[idx], nums[zero]
-            idx += 1
-                
-        
-            
+        insert_pos = 0
 
-        
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[insert_pos], nums[i] = nums[i], nums[insert_pos]
+                insert_pos += 1
