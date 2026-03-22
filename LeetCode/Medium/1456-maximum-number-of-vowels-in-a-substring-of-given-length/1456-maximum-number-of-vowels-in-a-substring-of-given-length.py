91a300fd9aa49ad54724348a9d25a58b77918855
Cheon <elioscode@gmail.com>
2026-03-22 18:02:50 +0900

    Time: 47 ms (84.73%), Space: 20.3 MB (7.73%) - LeetHub

---
+++ LeetCode/Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -0,0 +1,23 @@
+"""
+1. Approach
+    - using sliding window to calculate how many vowels in the substring.
+    - using True is 1 and False is 0 in python.
+2. Time Complexity : O(N) - check the elements of the String only once in Linear traversal
+3. Space Comeplxity : O(1) - String vowels to check if the character is vowel and `cur`, `ans` constant auxiliary space for checking
+"""
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels = "aeiou"
+
+        cur = sum([s[i] in vowels for i in range(k)])
+        ans = cur
+        for i in range(k, len(s)):
+            if ans == k: # answer can't exceed the length `k`, so early stopping condition
+                return k
+            
+            cur += (s[i] in vowels) - (s[i-k] in vowels)
+            if cur > ans:
+                ans = cur
+        
+        return ans
+        
