46c033814b33a5d4bed9d64c4784438f884bc8bf
Cheon <elioscode@gmail.com>
2026-03-26 15:01:01 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,20 +1,32 @@
 """
 1. Apporach
-    - I thought we need to compare each rows and cols to check whether they contain the same elements
-    - N is maximum 200 so, brute-force for checking each rows and col takes 2^4 * 10^7, slightly over 10^8 for 1 second.
-2. Time Complexity : O(N^4) - As I said before. But too time spending actually.
-3. Space Comeplexity : O(N) - Auxiliary space for column list.
+    - I use hash map, especially Counter class.
+    - Put all posible numbers which row can make in `counter` with the number of occurance.
+    - Make a query string with column and search that query in `counter`, if counter doesn't have query string, return 0
+2. Time Complexity : O(n^2) - I wrote detailed explanations above the code 
+3. Space Comeplexity : O(N) - For Counter and query stirng made with col need auxiliary space
 """
+from collections import Counter
+
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         ans = 0
+        k = []
+        # O(N^2) - every row, each elements in the row
+        for row in grid:
+            k.append("".join(str(c) for c in row))
+        # O(N)
+        counter = Counter(k)
+
+        # O(N^2)
         for j in range(len(grid)):
             col = []
+            # O(N)
             for i in range(len(grid)):
-                col.append(grid[i][j])
-            
-            for i in range(len(grid)):
-                if col == grid[i]:
-                    ans += 1
-        
+                col.append(str(grid[i][j]))
+            #O(N)
+            col_string = "".join(col)
+            #O(1)
+            ans += counter.get(col_string,0) # return 0 if there aren't col_string in counter
+
         return ans
