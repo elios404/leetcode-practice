9cffd6f173d1f99400a6195a9f39b30f20800954
Cheon <elioscode@gmail.com>
2026-04-11 08:32:34 +0900

    Time: 4 ms (35.6%), Space: 19.3 MB (84.15%) - LeetHub

---
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,57 @@
+"""
+1. Approach:
+    - Scan grid and save the rotten orange's locations by deque and how many fresh oranges exist.
+    - Use BFS in grid which moves 4-directionally adjacent.
+    - Before BFS, if there are none fresh orange, return 0 mins. And though while loop ended but if there are still fresh oranges, return -1.
+2. Time Complexity: O(M * N) - For scanning `grid` take O(M * N) and, in worst case every cell of grid can be putted in deque so while loop also takes O(M * N)
+3. Space Complexity: O(M * N) - For visited 2D-array need auxiliary O(M*N) and deque also need maximum O(M*N), and other `di`, `dj` or variable need constant auxiliary space.
+"""
+
+from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        di = [-1,1,0,0]
+        dj = [0,0,-1,1]
+
+        def checkBorder(i,j,m,n):
+            return 0<=i and i<m and 0<=j and j<n
+
+        m = len(grid)
+        n = len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+        rotten = deque()
+        fresh_cnt = 0
+
+        #search grid and find rotten orange's location and how many fresh oranges are there
+        # O(M * N)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    fresh_cnt += 1
+                elif grid[i][j] == 2:
+                    rotten.append((i,j,0)) #row, col, time
+                    visited[i][j] = True
+
+        if fresh_cnt == 0:
+            return 0
+
+        #O(V+E) ~= O(M * N)?
+        while rotten:
+            i, j, mins = rotten.popleft()
+
+            for d in range(4):
+                ni = i + di[d]
+                nj = j + dj[d]
+
+                if not checkBorder(ni,nj,m,n): continue # out of border
+                if visited[ni][nj]: continue # already visited
+
+                if grid[ni][nj] == 1: #if adjacent is fresh orange
+                    fresh_cnt -= 1
+                    if fresh_cnt == 0: # if all fresh oranges are rotten
+                        return mins+1
+                    visited[ni][nj] = True
+                    rotten.append((ni,nj,mins+1))
+        
+        return -1
