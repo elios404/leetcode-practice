4d5b806aaa1956b446e8a691682d5bb93e2584e0
Cheon <elioscode@gmail.com>
2026-04-12 00:53:45 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
+++ LeetCode/Medium/2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
@@ -1,27 +1,29 @@
 import heapq
 
 class SmallestInfiniteSet:
-
     def __init__(self):
-        self.heap_set = set()
-        for i in range(1, 1001):
-            self.heap_set.add(i)
+        # The pointer representing the lowest untouched number in the infinite set
+        self.current_min = 1
         
-        self.heap = list(self.heap_set)
+        # Only stores numbers that were explicitly added back (out-of-sequence)
+        self.added_back_heap = []
+        self.added_back_set = set() # For O(1) duplicate checking
 
     def popSmallest(self) -> int:
-        smallest = heapq.heappop(self.heap)
-        self.heap_set.remove(smallest)
+        # Priority 1: Check if any smaller numbers re-entered the pool
+        if self.added_back_heap:
+            smallest = heapq.heappop(self.added_back_heap)
+            self.added_back_set.remove(smallest)
+            return smallest
         
+        # Priority 2: Standard continuous sequence
+        smallest = self.current_min
+        self.current_min += 1
         return smallest
 
     def addBack(self, num: int) -> None:
-        if num not in self.heap_set:
-            self.heap_set.add(num)
-            heapq.heappush(self.heap, num)
-
-
-# Your SmallestInfiniteSet object will be instantiated and called as such:
-# obj = SmallestInfiniteSet()
-# param_1 = obj.popSmallest()
-# obj.addBack(num)
+        # We only care if the number is actually smaller than our pointer
+        # and not already sitting in our waiting pool.
+        if num < self.current_min and num not in self.added_back_set:
+            self.added_back_set.add(num)
+            heapq.heappush(self.added_back_heap, num)
