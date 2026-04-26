9b3d7845b7670e9ed10ec1017c1c41245cc6cd2d
Cheon <elioscode@gmail.com>
2026-04-26 18:36:44 +0900

    Time: 3 ms (19.61%), Space: 19.3 MB (36.86%) - LeetHub

---
+++ LeetCode/Medium/0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans = []
+
+        def makeCombination(num: int, picked: list, k: int, n: int):
+            if len(picked) == k and sum(picked) == n:
+                print("enter")
+                print(picked)
+                ans.append(list(picked))
+                return
+            
+            if num == 10 or len(picked) > k or sum(picked) > n:
+                return
+            
+            picked.append(num)
+            makeCombination(num+1, picked, k, n) # pick num
+            picked.pop()
+            makeCombination(num+1, picked, k, n) # pass num
+        
+        makeCombination(1, [], k, n)
+
+        return ans
