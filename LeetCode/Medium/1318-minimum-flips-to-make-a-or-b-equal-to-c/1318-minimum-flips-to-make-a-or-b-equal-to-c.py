b63c6aca00e149582c7f7ba88fbe9b00101dd959
Cheon <elioscode@gmail.com>
2026-04-28 15:24:06 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
+++ LeetCode/Medium/1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -1,14 +1,21 @@
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
         ret = 0
-        while a != 0 or b != 0 or c != 0:
-            a_right, b_right, c_right = a%2, b%2, c%2
-            ab = a_right|b_right
-            if ab != c_right: # need to change
-                if a_right == 1 and b_right == 1:
+        
+        while a > 0 or b > 0 or c > 0:
+            # Bitwise AND to extract the Right-most bit (LSB)
+            a_bit, b_bit, c_bit = a & 1, b & 1, c & 1
+            
+            if (a_bit | b_bit) != c_bit:
+                # Your brilliant logic remains exactly the same!
+                if a_bit == 1 and b_bit == 1:
                     ret += 2
                 else:
                     ret += 1
-            a, b, c = a//2, b//2, c//2
-        
+            
+            # Bitwise Right Shift to divide by 2 and push the next bit to the end
+            a >>= 1
+            b >>= 1
+            c >>= 1
+            
         return ret
