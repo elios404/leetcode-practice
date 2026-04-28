5cd6fad2e6667a554b254f517642bee347e880cb
Cheon <elioscode@gmail.com>
2026-04-28 11:47:28 +0900

    Time: 218 ms (11.99%), Space: 21.1 MB (12.83%) - LeetHub

---
+++ LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        #O(n log n)
+        piles.sort()
+        left, right = 1, piles[-1]
+
+        while left < right:
+            mid = (left+right)//2
+            print(left, mid, right)
+            cnt = 0 # time need to eat all bananas
+            for pile in piles:
+                cnt += (pile//mid)
+                cnt += 1 if pile%mid != 0 else 0
+            
+            if cnt <= h: #if k can be smaller
+                right = mid
+            else: # if k can be bigger
+                left = mid+1
+        
+        return left
+
+
+
+        
