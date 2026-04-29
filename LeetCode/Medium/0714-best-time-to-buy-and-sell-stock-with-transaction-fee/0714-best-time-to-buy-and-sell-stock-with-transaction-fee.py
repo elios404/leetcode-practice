4ea828ed7e9199d9b07dee3d7a07a2182d88e8db
Cheon <elioscode@gmail.com>
2026-04-29 18:09:59 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ LeetCode/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,26 +1,19 @@
-import functools
+MAX_N = int(5e4)
+MAX_PRICE = int(5e4)
+MAX_FEE = int(5e4)
+
+INF = MAX_N * MAX_PRICE * MAX_FEE
 
 
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
+        N = len(prices)
+        memo = [{True: 0, False: 0} for _ in range(N+1)]
 
-        @functools.lru_cache(maxsize=None)
-        def solve(i: int = 0, has_stock: bool = False) -> int:
-            if i == len(prices):
-                return 0
-            if has_stock:
-                return max(
-                    solve(i+1, False) + prices[i], # 판매
-                    solve(i+1, True),  # 존버
-                )
-            else:
-                return max(
-                    solve(i+1, True) - prices[i] - fee,  # 구매
-                    solve(i+1, False),  # 존버
-                )
-
-        for i in reversed(range(0, len(prices), 100)):
-            solve(i, False)
-            solve(i, True)
-
-        return solve()
+        memo[-1][True] = -INF
+        memo[-1][False] = 0
+        for i in range(N):
+            memo[i][True] = max(memo[i-1][True], memo[i-1][False]-prices[i]-fee)
+            memo[i][False] = max(memo[i-1][False], memo[i-1][True]+prices[i])
+        
+        return max(memo[N-1][True], memo[N-1][False])
