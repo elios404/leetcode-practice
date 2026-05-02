828157960409f93a8faa7e044473b03d1cc9b7e6
Cheon <elioscode@gmail.com>
2026-05-02 18:40:39 +0900

    Time: 0 ms (100%), Space: 19.3 MB (26.48%) - LeetHub

--- LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.py
+++ LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.py
@@ -1,23 +1,19 @@
-"""
-1. Apporach
-    - Using fast/slow pointer
-    - check whether s[idx] appears in t while linear traversal of String t
-2. Time Complexity : O(N + M) - len(t) as N and len(s) as M, Linear scan of both strings.
-3. Space Complexity : O(1) - constant auxiliary variable `idx` needed to check which character of s will be checked.
-"""
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        # At first, didn't put this if conditions. So failed
-        if not s: return True
-        if not t: return False
-        # Need to check the detail, about the range of variables.
+        if not t and not s:
+            return True
+        if not s:
+            return True
+        if not t:
+            return False
 
-        idx = 0
+        p, l = 0, len(s)
         for c in t:
-            if s[idx] == c:
-                idx += 1
-                if idx == len(s):
-                    return True
+            if s[p] == c:
+                p += 1
+            
+            if p == l:
+                return True
         
-        return idx >= len(s)
-        
+        return False
+
