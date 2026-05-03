6366f89bf9e99125fc11de0c385257aabeae7595
Cheon <elioscode@gmail.com>
2026-05-03 16:57:29 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.py
+++ LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.py
@@ -1,11 +1,7 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if not t and not s:
-            return True
         if not s:
             return True
-        if not t:
-            return False
 
         p, l = 0, len(s)
         for c in t:
