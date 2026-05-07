bbad6de3170c039974d346b54eed7651cc1711cc
Cheon <elioscode@gmail.com>
2026-05-07 17:52:36 +0900

    Time: 22 ms (97.98%), Space: 25.9 MB (9.98%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -8,13 +8,14 @@ class Solution:
         def findMinOps(i, j):
             if i == l1 or j == l2:
                 return max(l1-i, l2-j)
-            elif word1[i] == word2[j]:
+            
+            if word1[i] == word2[j]:
                 return findMinOps(i+1, j+1)
-            else:
-                return min(
-                    findMinOps(i+1, j), #remove
-                    findMinOps(i, j+1), # insert
-                    findMinOps(i+1, j+1) # replace
-                ) + 1
+            
+            return min(
+                findMinOps(i+1, j), #remove
+                findMinOps(i, j+1), # insert
+                findMinOps(i+1, j+1) # replace
+            ) + 1
                 
         return findMinOps(0,0)
