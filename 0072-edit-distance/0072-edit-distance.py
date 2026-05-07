0d0a0b05c2ea6dd8577738ab5f54bd57176e49c5
Cheon <elioscode@gmail.com>
2026-05-07 17:23:45 +0900

    Time: 31 ms (90.32%), Space: 26.1 MB (9.26%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,20 @@
+from functools import cache 
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        l1, l2 = len(word1), len(word2)
+
+        @cache 
+        def findMinOps(i, j):
+            if i == l1 or j == l2:
+                return max(l1-i, l2-j)
+            elif word1[i] == word2[j]:
+                return findMinOps(i+1, j+1)
+            else:
+                return min(
+                    findMinOps(i+1, j), #remove
+                    findMinOps(i, j+1), # insert
+                    findMinOps(i+1, j+1) # replace
+                ) + 1
+                
+        return findMinOps(0,0)
