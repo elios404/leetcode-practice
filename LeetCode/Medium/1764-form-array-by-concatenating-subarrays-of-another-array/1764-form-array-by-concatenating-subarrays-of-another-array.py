c3d8fd39cfcb4f9c1b4986ae0fa903e5137be17e
Cheon <elioscode@gmail.com>
2026-05-13 17:29:18 +0900

    Time: 3 ms (65.18%), Space: 19.5 MB (41.96%) - LeetHub

--- LeetCode/Medium/1764-form-array-by-concatenating-subarrays-of-another-array/1764-form-array-by-concatenating-subarrays-of-another-array.py
+++ LeetCode/Medium/1764-form-array-by-concatenating-subarrays-of-another-array/1764-form-array-by-concatenating-subarrays-of-another-array.py
@@ -1,3 +1,19 @@
+"""
+Constraints:
+
+groups.length == n
+1 <= n <= 10^3
+1 <= groups[i].length, sum(groups[i].length) <= 10^3
+1 <= nums.length <= 10^3
+-10^7 <= groups[i][j], nums[k] <= 10^7
+
+1. Approach
+    - Using two pointer method, when num in nums are same in current group's k'th element. Then widen the range.
+    - If it's not same, then increase start point index and set `move` back to 0
+    - if `move` is same with current group's length, then this subarray is equal with current group, so move to next group
+2. Time Complexity : O(N * M) - N is the length of `nums` and M is maximum length of a group in groups.
+3. Space Complexity : O(1) - Constant space for `gIdx`, `start`, `move` are needed.
+"""
 class Solution:
     def canChoose(self, groups: List[List[int]], nums: List[int]) -> bool:
         gIdx = 0
