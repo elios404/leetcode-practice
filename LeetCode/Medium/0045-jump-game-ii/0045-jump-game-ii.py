1172d5d64d5ff5ac5e8290e376a3573569129d5f
Cheon <elioscode@gmail.com>
2026-05-13 13:12:20 +0900

    Time: 3 ms (89.35%), Space: 20 MB (66.61%) - LeetHub

--- LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.py
+++ LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,27 +1,18 @@
-"""
-1. Approach:
-    - Solve it with DP, and write code as problem explains.
-2. Time Complexity : O(N^2) - But actaully it takes 13483ms and most of people solved it in 32ms
-3. Space Complexity : O(N) - @cache need O(N) I think.
-
-Conclusion : It works, but super slow
-"""
-from functools import cache
-
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        steps = 0
+        cur_idx = reachable_range = 0
 
-        @cache
-        def find_route(idx: int) -> int:
-            if idx == 0:
-                return 0
+        while reachable_range < n-1:
+            # print(steps, cur_idx, reachable_range)
+            next_reachable_range = 0
+            for i in range(cur_idx, reachable_range+1):
+                next_reachable_range = max(next_reachable_range, i+nums[i])
+            
+            steps += 1
+            cur_idx = reachable_range+1
+            reachable_range = next_reachable_range
+            
 
-            min_value = float('inf')
-            front = idx-1000 if idx-1001>-1 else -1
-            for i in range(idx-1, front, -1):
-                if idx - i <= nums[i]: # can approach
-                    min_value = min(min_value, find_route(i)+1)
-
-            return min_value
-        
-        return find_route(len(nums)-1)        
+        return steps
