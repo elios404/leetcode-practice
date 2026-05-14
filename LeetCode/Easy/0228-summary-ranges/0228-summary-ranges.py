6fb33919fb42af4467c2b2b075ca59314fa6d231
Cheon <elioscode@gmail.com>
2026-05-15 08:30:29 +0900

    Time: 0 ms (100%), Space: 19.3 MB (62.99%) - LeetHub

--- LeetCode/Easy/0228-summary-ranges/0228-summary-ranges.py
+++ LeetCode/Easy/0228-summary-ranges/0228-summary-ranges.py
@@ -2,23 +2,17 @@ class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         if not nums:
             return []
-        range_tuples = []
-        start = end = nums[0]
+        range_strings = []
+        s = e = nums[0]
 
         # O(N)
         for i in range(1, len(nums)):
-            if nums[i] - 1 == end:
-                end = nums[i]
+            if nums[i] - 1 == e:
+                e = nums[i]
                 continue
             else:
-                range_tuples.append((start, end))
-                start = end = nums[i]
-        range_tuples.append((start,end))
-
-        ret = []
-        # O(N)
-        for s, e in range_tuples:
-            r = f"{s}->{e}" if s != e else f"{s}"
-            ret.append(r)
+                range_strings.append(f"{s}->{e}" if s != e else f"{s}")
+                s = e = nums[i]
+        range_strings.append(f"{s}->{e}" if s != e else f"{s}")
         
-        return ret
+        return range_strings
