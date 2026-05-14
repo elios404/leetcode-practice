67efb070d07fc8dcb00ad2a05b3f78ab5e51c39c
Cheon <elioscode@gmail.com>
2026-05-14 15:39:30 +0900

    Time: 16 ms (64.88%), Space: 19.6 MB (72.72%) - LeetHub

--- LeetCode/Easy/0383-ransom-note/0383-ransom-note.py
+++ LeetCode/Easy/0383-ransom-note/0383-ransom-note.py
@@ -2,11 +2,9 @@ from collections import Counter
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        note_count = Counter(ransomNote)
-        magazine_count = Counter(magazine)
-
-        for k in note_count:
-            if k not in magazine_count or note_count[k] > magazine_count[k]:
-                return False
-
-        return True
+        # Early exit optimization
+        if len(ransomNote) > len(magazine):
+            return False
+            
+        # Counter subtraction: returns an empty Counter if magazine has enough letters
+        return not (Counter(ransomNote) - Counter(magazine))
