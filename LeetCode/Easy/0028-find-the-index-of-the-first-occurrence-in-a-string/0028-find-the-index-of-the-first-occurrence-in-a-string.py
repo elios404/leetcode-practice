67fbe4d285cdfdbc73b5d25a0eb739002e6379dd
Cheon <elioscode@gmail.com>
2026-05-14 16:54:58 +0900

    Time: 0 ms (100%), Space: 19.6 MB (8.73%) - LeetHub

--- LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,11 +1,44 @@
-# 1. brute force way
+# 2. KMP algorithm
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        needle_point = 0
-        n = len(needle)
-
-        for i, c in enumerate(haystack):
-            if haystack[i:i+n] == needle:
-                return i
         
-        return -1
+        def compute_lps(pattern)-> list:
+            lps = [0] *len(pattern)
+            length = 0
+            i = 1
+
+            while i < len(pattern):
+                if pattern[i] == pattern[length]:
+                    length += 1
+                    lps[i] = length
+                    i += 1
+                else:
+                    if length > 0:
+                        length = lps[length - 1]
+                    else:
+                        lps[i] = 0
+                        i += 1
+            
+            return lps
+        
+        def kmp_search(text, pattern)-> int:
+            lps = compute_lps(pattern)
+
+            i, j = 0, 0
+
+            while i < len(text):
+                if text[i] == pattern[j]:
+                    i += 1
+                    j += 1
+                    if j == len(pattern):
+                        print(f"start point of pattern : {i - j}")
+                        return i-j
+                else:
+                    if j > 0:
+                        j = lps[j - 1]
+                    else:
+                        i += 1
+            
+            return -1
+        
+        return kmp_search(haystack, needle)
