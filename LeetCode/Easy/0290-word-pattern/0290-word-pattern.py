8b090c1503623a702edcdbfaace3eb460fd6b39c
Cheon <elioscode@gmail.com>
2026-05-20 22:44:42 +0900

    Time: 0 ms (100%), Space: 19.3 MB (29.18%) - LeetHub

--- LeetCode/Easy/0290-word-pattern/0290-word-pattern.py
+++ LeetCode/Easy/0290-word-pattern/0290-word-pattern.py
@@ -1,12 +1,18 @@
+# I want to make a prefect algorithm code, but always make it with try and found error. I'm worried these kind of things are not good cuz
+# In real world, it's hard to do test, can't keep submit easily, and also failure need money also.
+# I want to be more stable developer
+
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         pattern_dict = {}
         word_dict = {}
         words = s.split(" ")
 
+        # found from failure, failed submission
         if len(pattern) != len(words):
             return False
 
+        #found from failure, failed submission
         for p, word in zip(pattern, words):
             # print(pattern_dict, word_dict)
             if p in pattern_dict and pattern_dict[p] != word:
