3552289a22784541fd381b302e59b683e1b92783
Cheon <elioscode@gmail.com>
2026-06-02 23:00:52 +0900

    Time: 0 ms (100%), Space: 19.5 MB (5.52%) - LeetHub

--- LeetCode/Easy/0202-happy-number/0202-happy-number.py
+++ LeetCode/Easy/0202-happy-number/0202-happy-number.py
@@ -1,24 +1,21 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        calculated_already = set()
+        calced = set()
 
         def process(number: int, s: set) -> bool:
-            each_num = []
+            next_number = 0
             while number != 0:
-                each_num.append(number%10)
+                next_number += (number%10)**2
                 number = number//10
             
-            processed_number = 0
-            for num in each_num:
-                processed_number += num**2
-            if processed_number == 1:
+            if next_number == 1:
                 return True
-            
-            if processed_number in s:
+
+            if next_number in s:
                 return False
             else:
-                s.add(processed_number)
-                return process(processed_number, s)
+                s.add(next_number)
+                return process(next_number, s)
         
-        return process(n, calculated_already)
+        return process(n, calced)
         
