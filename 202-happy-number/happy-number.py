0e261604f3ec5229a466ed69e1e12a65265f944f
Cheon <elioscode@gmail.com>
2026-06-02 23:29:33 +0900

    Time: 3 ms (38.73%) | Memory: 19.3 MB (22.86%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,23 @@
+# Use DP pattern to solve this. If same number appears again, then it makes endless loop so, return False
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        calced = set()
+
+        def process(number: int, s: set) -> bool:
+            next_number = 0
+            while number != 0:
+                next_number += (number%10)**2
+                number = number//10
+            
+            if next_number == 1:
+                return True
+
+            if next_number in s:
+                return False
+            else:
+                s.add(next_number)
+                return process(next_number, s)
+        
+        return process(n, calced)
+        
