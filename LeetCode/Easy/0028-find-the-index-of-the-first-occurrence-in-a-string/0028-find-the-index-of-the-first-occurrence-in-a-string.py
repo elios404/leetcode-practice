26ca66c77c4fe5d3a214462e913b0ebd55c835d2
Cheon <elioscode@gmail.com>
2026-06-07 20:24:18 +0900

    Time: 2 ms (24.15%), Space: 19.5 MB (8.84%) - LeetHub

--- LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,44 +1,49 @@
-# 2. KMP algorithm
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         
-        def compute_lps(pattern)-> list:
-            lps = [0] *len(pattern)
+        def compute_lps(pattern: str)-> list:
+            m = len(pattern)
+            lps = [0] * m
             length = 0
             i = 1
 
-            while i < len(pattern):
+            while i < m:
                 if pattern[i] == pattern[length]:
                     length += 1
                     lps[i] = length
                     i += 1
                 else:
-                    if length > 0:
+                    if length != 0:
                         length = lps[length - 1]
                     else:
                         lps[i] = 0
                         i += 1
             
             return lps
-        
-        def kmp_search(text, pattern)-> int:
+
+        def kmp_search(text: str, pattern: str)-> int:
+            n = len(text)
+            m = len(pattern)
             lps = compute_lps(pattern)
 
-            i, j = 0, 0
+            i = 0
+            j = 0
+            result = [] 
 
-            while i < len(text):
-                if text[i] == pattern[j]:
+            while i < n:
+                if pattern[j] == text[i]:
                     i += 1
                     j += 1
-                    if j == len(pattern):
-                        print(f"start point of pattern : {i - j}")
-                        return i-j
+                
+                    if j == m:
+                        return i - j
+                
                 else:
-                    if j > 0:
-                        j = lps[j - 1]
+                    if j != 0:
+                        j = lps[j-1]
                     else:
                         i += 1
             
             return -1
-        
+
         return kmp_search(haystack, needle)
