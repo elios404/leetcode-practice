3937460a1fbb7f716f927606622964aea416b57e
Cheon <elioscode@gmail.com>
2026-08-05 22:10:06 +0930

    Time: 0 ms (100%), Space: 19.4 MB (10.7%) - LeetHub

--- LeetCode/Medium/0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ LeetCode/Medium/0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -2,15 +2,19 @@ from collections import deque
 
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        if endGene not in bank:
+            return -1
+
         gene_type = ['A','C','G','T']
         visited = set()
+        bank = set(bank)
         process = deque()
         process.append((startGene, 0)) #current Gene, steps
+        visited.add(startGene)
 
         while process:
             curr_gene, steps = process.popleft()
-            visited.add(curr_gene)
-
+            
             for i in range(8):
                 for tp in gene_type:
                     if curr_gene[i] == tp: # nothing changed
@@ -21,6 +25,7 @@ class Solution:
                         if next_gene == endGene:
                             return steps + 1
                         else:
+                            visited.add(next_gene)
                             process.append((next_gene, steps + 1))
 
         return -1
