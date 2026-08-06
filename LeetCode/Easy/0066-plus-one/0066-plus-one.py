6cc6a2e8a884a7aded90da4a2108be92fb83d3f6
Cheon <elioscode@gmail.com>
2026-08-06 09:56:52 +0930

    Time: 0 ms (100%), Space: 19.3 MB (18.53%) - LeetHub

--- LeetCode/Easy/0066-plus-one/0066-plus-one.py
+++ LeetCode/Easy/0066-plus-one/0066-plus-one.py
@@ -1,18 +1,10 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        carry = 0
-        digits[len(digits)-1] += 1 #add 1 
-
-        for i in range(len(digits)-1, -1, -1):
-            new = digits[i] + carry
-            if new == 10:
-                carry = 1
-                digits[i] = 0
-            else:
-                carry = 0
-                digits[i] = new
-        
-        if carry == 1:
-            digits = [1] + digits
-        
-        return digits
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits  # 올림이 없으므로 바로 종료
+            digits[i] = 0
+            
+        # 루프를 다 돌았는데도 return되지 않았다면 (예: [9, 9, 9] -> [0, 0, 0])
+        return [1] + digits
