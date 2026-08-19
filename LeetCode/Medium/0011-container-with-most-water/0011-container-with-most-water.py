b486f00f0db27efd4abe2a933986dd7077e51450
Cheon <elioscode@gmail.com>
2026-08-19 10:17:50 +0930

    Time: 51 ms (85.85%), Space: 29.9 MB (5.35%) - LeetHub

--- LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.py
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,25 +1,15 @@
-"""
-1. Approach
-    - Two pointer from start and end of the List
-    - Find maximun area, while moving pointers
-    - If `front`'s height is smaller, to find bigger go forward, in opposite case, go backward
-2. Time Complexity : O(N) - Scan list `height` once in the worse case.
-3. Space Complexity : O(1) - constant auxiliary space for pointers needed.
-"""
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        front = 0
-        back = len(height)-1
-
-        ans = 0
+        front, back = 0, len(height)-1
+        max_water = 0
+        
         while front < back:
-            h = min(height[front], height[back])
-            w = back-front
-            ans = max(ans, h*w)
+            water = min(height[front], height[back]) * (back-front)
+            max_water = max(max_water, water)
+            
             if height[front] < height[back]:
                 front += 1
             else:
                 back -= 1
         
-        return ans
-        
+        return max_water
